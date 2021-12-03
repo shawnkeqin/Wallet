4769ae8ff12b862d91c43a401c804a02c0350ce7
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 03:37:32 +0800

    subscription resolver

--- server/src/transactions/transactions.resolver.ts
+++ server/src/transactions/transactions.resolver.ts
@@ -2,12 +2,15 @@ import { Resolver, Query, Mutation, Args, Subscription } from '@nestjs/graphql';
 import { TransactionsService } from './transactions.service';
 import { UpdateTransactionInput } from './dto/transaction.input';
 import { Transaction } from './entities/transaction.entity';
+import { Inject } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
 
 
 @Resolver(() => Transaction)
 export class TransactionsResolver {
     constructor(
         private readonly transactionsService: TransactionsService,
+        @Inject('PUB_SUB') private readonly pubSub: PubSub,
       ) {}
 
       @Query(() => [Transaction], { name: 'transactions' })
@@ -21,6 +24,11 @@ export class TransactionsResolver {
         updateInput: UpdateTransactionInput,
       ) {
         return this.transactionsService.update(updateInput);
+      }
+
+      @Subscription(() => [Transaction], { nullable: true })
+      async transactionsAdded() {
+        return this.pubSub.asyncIterator<Transaction>('transactionsAdded');
       }
 
 }
