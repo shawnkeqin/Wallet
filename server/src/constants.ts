99e3269782f29e20e0928cabc27f165c686c7ed3
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 03:24:40 +0800

    data sync service

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,3 +4,5 @@ export const BEZOS_RELATED_COMPANIES = [
   'Whole Foods',
   'Blue Origin',
 ];
+export const COMPANY_TRANSACTIONS_URL =
+  'https://61a5240d4c822c00170420e8.mockapi.io/v1/companies';
--- server/src/transactions/transactions.service.ts
+++ server/src/transactions/transactions.service.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
-import { Repository } from 'typeorm';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { In, Repository } from 'typeorm';
+import { BEZOS_RELATED_COMPANIES, COMPANY_TRANSACTIONS_URL } from '../constants';
+import { HttpService } from '@nestjs/axios';
+import { PubSub } from 'graphql-subscriptions';
 import {
   IMappedTransaction,
   IOriginalTransaction,
 } from './dto/transaction.input';
-import { BEZOS_RELATED_COMPANIES } from '../constants';
+
 
 
 @Injectable()
 export class TransactionsService {
+  private readonly logger = new Logger(TransactionsService.name);
     constructor(
         @InjectRepository(Transaction)
         private transactionRepository: Repository<Transaction>,
+        private httpService: HttpService,
+        @Inject('PUB_SUB') private readonly pubSub: PubSub,
       ) {}
 
       findAll(): Promise<Transaction[]> {
@@ -30,5 +37,39 @@ export class TransactionsService {
         await this.transactionRepository.update({ merchant_name }, { isFollowed });
         return this.transactionRepository.find({ order: { id: 'ASC' } });
       }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async handleCron() {
+    this.logger.debug('========Called when the current second is 10========');
+    const response = await this.httpService
+      .get<IOriginalTransaction[]>(COMPANY_TRANSACTIONS_URL)
+      .toPromise();
+    const existingData = await this.transactionRepository.find({
+      where: { id: In(response.data.map((item) => item.id)) },
+    });
+    const data: IMappedTransaction[] = response.data.reduce(
+      (prev: IMappedTransaction[], curr: IOriginalTransaction) => {
+        if (existingData.some((oldItem) => oldItem.id === curr.id)) return prev;
+        return prev.concat({
+          ...curr,
+          isFollowed: BEZOS_RELATED_COMPANIES.includes(curr.merchant_name),
+        });
+      },
+      [],
+    );
+    if (!data.length) {
+      this.logger.verbose('Not found new data to insert into db');
+      return;
+    }
+    await this.transactionRepository.insert(data);
+    this.logger.verbose(`Inserted data: ${data.length}`);
+    const transactionsAdded = await this.transactionRepository.find({
+      where: { id: In(data.map((item) => item.id)) },
+      order: { id: 'ASC' },
+    });
+    await this.pubSub.publish('transactionsAdded', {
+      transactionsAdded,
+    });
+  }
 
 }
