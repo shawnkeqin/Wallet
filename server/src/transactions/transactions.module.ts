b48f470708567cd896e5d9e97c6caae5a0d19352
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 03:44:46 +0800

    module imports

--- server/src/transactions/transactions.module.ts
+++ server/src/transactions/transactions.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
+import { TransactionsResolver } from './transactions.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { HttpModule } from '@nestjs/axios';
+import { PubSub } from 'graphql-subscriptions';
 
 @Module({
   imports: [],
-  providers: [TransactionsService],
+  providers: [TransactionsResolver, TransactionsService],
 })
 export class TransactionsModule {}
