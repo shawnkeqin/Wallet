7ab3357b61cf9a00f51043b2481262371b6c0a00
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 18:23:29 +0800

    add server config again due to unsaved files

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+(async () => {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  const port = +configService.get<number>('PORT', 80);
+  await app.listen(port);
+})();
