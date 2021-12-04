ef4f620a655be2cca701f42ccf5b163678c3f3f9
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 18:18:32 +0800

    add graphql-subscription again in app module due to previous unsaved file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
+import { Transaction } from './transactions/entities/transaction.entity';
+import { ScheduleModule } from '@nestjs/schedule';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': {
+          path: '/graphql',
+        },
+      },
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        name: 'dev',
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<string>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        schema: 'public',
+        entities: [Transaction],
+        synchronize: true,
+      }),
+    }),
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
