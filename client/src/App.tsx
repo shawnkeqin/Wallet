d29d28663d38fa091c83b27b92d64183ac9004a3
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 18:40:00 +0800

    add uncommited changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Transactions from "./components/TransactionsComponent/Transactions";
 
 function App() {
   return (
     <div className="App">
-Hello World
+<Transactions />
     </div>
   );
 }
