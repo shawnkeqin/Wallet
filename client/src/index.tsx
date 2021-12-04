cb016f3684ffc67819e3dc0aa080045078738318
Shawn Lim <e0052482@u.nus.edu>
2021-12-04 14:48:56 +0800

    add websocket connection graphql subscription and backend link

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+import {split, HttpLink} from '@apollo/client';
+import {getMainDefinition} from '@apollo/client/utilities';
+import {WebSocketLink} from '@apollo/client/link/ws';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URL,
+});
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_WEBSOCKET_URL || '',
+  options: {
+    reconnect: true,
+  }
+});
+
+const splitLink = split(
+  ({query}) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
+      <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
