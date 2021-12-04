f592004f301a09a1835c50619ece3298e87d2f5c
Shawn Lim <e0052482@u.nus.edu>
2021-12-05 05:14:16 +0800

    add test to spec

--- server/src/transactions/transactions.service.spec.ts
+++ server/src/transactions/transactions.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { TransactionsService } from './transactions.service';
+import { Transaction } from './entities/transaction.entity';
+import { HttpService } from '@nestjs/axios';
+import { PubSub } from 'graphql-subscriptions';
+import { HttpModule } from '@nestjs/axios';
+
+const mockTransactionsRepository = {};
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      imports: [HttpModule],
+      providers: [TransactionsService,{
+        provide: 'PUB_SUB',
+        useValue: new PubSub(),
+      },
+      {
+        provide: getRepositoryToken(Transaction),
+        useValue: mockTransactionsRepository,
+      }
+    ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
